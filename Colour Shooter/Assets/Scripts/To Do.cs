648eef6b7bd97e7c9c58f9d3d8776c855a842e5d
Diana <edvi.diana@gmail.com>
2025-02-02 23:27:48 -0500

    Added trampoline sprite

---
+++ Colour Shooter/Assets/Scripts/To Do.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToDo : MonoBehaviour
+{
+    // To do
+    // Material for composite objects 
+    // Phasing in and out 
+    // 
+}
